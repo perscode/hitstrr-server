35ce6f5e6cc9919700f73032e554e725863a686d
flanders-pixel <flanderslind@gmail.com>
2026-06-22 22:17:43 +0200

    Fix server stability

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 const DATA_FILE = path.join(__dirname, 'playlists.json');
